82ce907aa00a803d6f27e8b98d72222cff588d25
aswinrajpv <aswinraj1633@gmail.com>
2025-11-04 09:24:40 +0530

    later some

---
+++ Pmt_Admin.Test/Handlers/Du/GetAllDuQueryHandlerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pmt_Admin.Test.Handlers.Du
+{
+    internal class GetAllDuQueryHandlerTest
+    {
+    }
+}

f8d2b601c2b5c6f6dcecf6031af42e158b3a9fab
Alan Jose <alnjoze@gmail.com>
2025-11-04 17:54:11 +0530

    fix: added fixes

--- PmtAdmin.Infrastructure/Services/Jira/JiraDatabaseService.cs
+++ PmtAdmin.Infrastructure/Services/Jira/JiraDatabaseService.cs
@@ -27,7 +27,8 @@ namespace PmtAdmin.Infrastructure.Services.Jira
             var returnUsers = new List<User>();
             var returnProjects = new List<Project>();
 
-            var issueStatuses = new List<string> { "To Do", "In Progress", "Done" };
+            var issueStatuses = new List<string> { "TO_DO", "IN_PROGRESS", "DONE" };
+
             var existingStatuses = _context.Statuses
                 .Where(s => issueStatuses.Contains(s.StatusName))
                 .Select(s => s.StatusName)
@@ -102,6 +103,8 @@ namespace PmtAdmin.Infrastructure.Services.Jira
                     bool pmExists = await _context.ProjectMembers
                         .AnyAsync(pm => pm.ProjectId == p.Id && pm.UserId == user.Id);
 
+                    int pm_id = 0;
+
                     if (!pmExists)
                     {
                         var pm = new ProjectMember
@@ -111,6 +114,7 @@ namespace PmtAdmin.Infrastructure.Services.Jira
                             RoleId = PmRoleId //2 for PM Role
                         };
                         ProjectMembers.Add(user.Id);
+                        pm_id = pm.Id;
                         await _context.ProjectMembers.AddAsync(pm);
                         await _context.SaveChangesAsync();
                     }
@@ -186,7 +190,8 @@ namespace PmtAdmin.Infrastructure.Services.Jira
                         var t = new Team
                         {
                             Name = b.Name + " Team",
-                            ProjectId = p.Id
+                            ProjectId = p.Id,
+                            LeadId = pm_id
                         };
 
                         _context.Teams.Add(t);
@@ -209,7 +214,7 @@ namespace PmtAdmin.Infrastructure.Services.Jira
                                 sp.TeamId = t.Id;
                                 sp.Status = JiraSprintStateToProjectStatusMappingScheme.ContainsKey(sprint.State)
                                     ? JiraSprintStateToProjectStatusMappingScheme[sprint.State]
-                                    : "Planned";
+                                    : "PLANNED";
                                 sprints.Add(sp);
                                 SprintEntityJiraSprintModelMappingScheme[sprint.Id] = sp.Id.ToString();
                             }
@@ -237,6 +242,7 @@ namespace PmtAdmin.Infrastructure.Services.Jira
 
                                 i.ProjectId = p.Id;
                                 i.Title = issue.Summary;
+                                i.Type = issue.IssueType.Name.ToUpper();
 
                                 if (issue.Sprint != null)
                                     i.SprintId = SprintEntityJiraSprintModelMappingScheme.ContainsKey(issue.Sprint.Id)
@@ -256,7 +262,7 @@ namespace PmtAdmin.Infrastructure.Services.Jira
                                     _context.BoardColumns.Add(boardcolumn);
                                     await _context.SaveChangesAsync();
 
-                                    var tempStatusName = issue.Status.Name.ToUpper().Replace(" ", "");
+                                    var tempStatusName = issue.Status.Name.ToUpper().Replace(" ", "_");
                                     var searchedStatus = allStatuses.Find(s => s.StatusName == tempStatusName);
 
                                     Status statusToUse;
